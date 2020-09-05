cc754b739cd27e9281363f86cd105a896ad41356
abacobob <oowdeck@yahoo.co.uk>
2020-09-05 13:33:46 +0200

    new start script

---
+++ demo-app/build/precache-manifest.1b42b5020a0898e5370e30de0795e0ab.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71c7ab78198ef3e7c0e0b632098b10f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07154fe9c76f5d6b1e2c",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "b084bb4be52f787c1495",
+    "url": "/static/css/main.946573e9.chunk.css"
+  },
+  {
+    "revision": "07154fe9c76f5d6b1e2c",
+    "url": "/static/js/2.887edc34.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.887edc34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b084bb4be52f787c1495",
+    "url": "/static/js/main.4142d9fe.chunk.js"
+  },
+  {
+    "revision": "4390a351b0f90a7c34c6",
+    "url": "/static/js/runtime-main.8bc1365d.js"
+  },
+  {
+    "revision": "a802a29eb033cb699c007b704eaede15",
+    "url": "/static/media/asturias.a802a29e.jpg"
+  }
+]);
